c1ccd0e0a1a6e536de715f3928036051f0091ad5
Sqyd <wojkap10@proton.me>
2025-09-08 12:53:05 +0200

    Kakalko makes tuntuntunsahur go away:(

---
+++ src/semi3d/doom/mapmaker/Mapmaker.c
@@ -0,0 +1,13 @@
+#include <raylib.h>
+
+int main() {
+	InitWindow(800, 400, "Mapmaker");
+	SetTargetFPS(60);
+
+	while(!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(WHITE);
+
+		EndDrawing();
+	}
+}

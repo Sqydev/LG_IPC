b024e946d1d28ff24a87b267c4a6d8839c0b9383
Sqyd <wojkap10@proton.me>
2025-09-09 20:04:51 +0200

    Mapmaker

--- src/semi3d/doom/mapmaker/Mapmaker.c
+++ src/semi3d/doom/mapmaker/Mapmaker.c
@@ -1,10 +1,22 @@
 #include <raylib.h>
+#include <stdlib.h>
 
 #define screenWidth 800
 #define screenHeight 400
+	
+typedef struct Sector {
+		float FloorHeight;
+		float CeilingHeight;
+		char FloorTexturePath[200];
+		char CeilingTexturePath[200];
+		float LightLevel;
+		int SideDefsNumb;
+		Vector2 SideDefsCords[100];
+	} Sector;
 
-void CreateSector(Vector2 newSectorsPos) {
-	// You know what's here
+int NumbOfMapSectors = 0;
+
+void CreateSector(Vector2 newSectorsPos, Sector *MapSectors) {
 }
 
 int main() {
@@ -16,14 +28,13 @@ int main() {
 	camera.target = (Vector2){0, 0};
 	camera.offset = (Vector2){(float)screenWidth / 2, (float)screenHeight / 2};
 	camera.zoom = 1.0f;
+	
+	Sector *MapSectors = malloc(0 * sizeof(Sector));
 
 	while(!WindowShouldClose()) {
 		// Get mouse position in the WORLD's perspective(not screen's perspective)
 		Vector2 mousePosInWorld = GetScreenToWorld2D(GetMousePosition(), camera);
 
-		if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) { 
-			CreateSector(mousePosInWorld);
-		}
 
 		BeginDrawing();
 		ClearBackground(WHITE);
